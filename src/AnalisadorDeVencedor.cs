1e3d5f33d931e735e1ad6f6b5b3bf4696bbb3b23
Matheus Bertonha <bertonha_2011@hotmail.com>
2020-03-11 15:44:53 -0300

    Melhorias na regra de definição do vencedor

--- src/AnalisadorDeVencedor.cs
+++ src/AnalisadorDeVencedor.cs
@@ -12,16 +12,72 @@ namespace Desafio_Poker_Simples.src
 
         public string Analisar(List<string> cartasJogador1, List<string> cartasJogador2)
         {
+            var cartasDuplicadosDoJogador1 = cartasJogador1
+                .Select(carta => ConverterParaValorDaCarta(carta))
+                .GroupBy(valorDaCarta => valorDaCarta)
+                .Where(grupo => grupo.Count() > 1);
+
+            var cartasDuplicadosDoJogador2 = cartasJogador2
+                .Select(carta => ConverterParaValorDaCarta(carta))
+                .GroupBy(valorDaCarta => valorDaCarta)
+                .Where(grupo => grupo.Count() > 1);
+
+            if (cartasDuplicadosDoJogador1 != null && cartasDuplicadosDoJogador1.Any() &&
+                    cartasDuplicadosDoJogador2 != null && cartasDuplicadosDoJogador2.Any())
+            {
+                var maiorParDeCartasDoJogador1 = cartasDuplicadosDoJogador1.Select(valor => valor.Key)
+                    .OrderBy(valor => valor)
+                    .Max();
+                var maiorParDeCartasDoJogador2 = cartasDuplicadosDoJogador2.Select(valor => valor.Key)
+                    .OrderBy(valor => valor)
+                    .Max();
+                return maiorParDeCartasDoJogador1 > maiorParDeCartasDoJogador2 ?
+                    "Jogador 1" : "Jogador 2";
+            }
+            else if (cartasDuplicadosDoJogador1 != null && cartasDuplicadosDoJogador1.Any())
+            {
+                return "Jogador 1";
+            }
+            else if (cartasDuplicadosDoJogador2 != null && cartasDuplicadosDoJogador2.Any())
+            {
+                return "Jogador 2";
+            }
+
             var maiorCartaDoJogador1 = cartasJogador1
-                .Select(carta => int.Parse(carta.Substring(0, 1)))
+                .Select(carta => ConverterParaValorDaCarta(carta))
                 .OrderBy(valorDaCarta => valorDaCarta)
                 .Max();
+
             var maiorCartaDoJogador2 = cartasJogador2
-                .Select(carta => int.Parse(carta.Substring(0, 1)))
+                .Select(carta => ConverterParaValorDaCarta(carta))
                 .OrderBy(valorDaCarta => valorDaCarta)
                 .Max();
 
             return maiorCartaDoJogador1 > maiorCartaDoJogador2 ? "Jogador 1" : "Jogador 2";
+        }
+
+        private int ConverterParaValorDaCarta(string carta)
+        {
+            var valorDaCarta = carta.Substring(0, carta.Length - 1);
+            if (!int.TryParse(valorDaCarta, out var valor))
+            {
+                switch (valorDaCarta)
+                {
+                    case "V":
+                        valor = 11;
+                        break;
+                    case "D":
+                        valor = 12;
+                        break;
+                    case "R":
+                        valor = 13;
+                        break;
+                    case "A":
+                        valor = 14;
+                        break;
+                }
+            }
+            return valor;
         }
     }
 }
--- test/AnalisadorDeVencedorTest.cs
+++ test/AnalisadorDeVencedorTest.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections.Generic;
 using Desafio_Poker_Simples.src;
 using Xunit;
@@ -6,25 +7,14 @@ namespace Desafio_Poker_Simples.test
 {
     public class AnalisadorDeVencedorTest
     {
-        [Fact]
-        public void DeveAnalisarVencedrorQuandoJogador1TiverMaiorCarta()
+        [Theory]
+        [InlineData("3O,5C,2E,9C,7P", "2O,2C,3P,6C,7C", "Jogador 2")]
+        [InlineData("DC,DO,3P,6C,7C", "3O,5C,2E,9C,7P", "Jogador 1")]
+        public void DeveAnalisarVencedrorQuandoJogadorTiverMaiorCarta(string cartasDoJogador1String, string cartasDoJogador2String, string vencedorEsperado)
         {
-            const string vencedorEsperado = "Jogador 1";
-            var cartasJogador1 = new List<string> { "3O", "5C", "2E", "9C", "7P" };
-            var cartasJogador2 = new List<string> { "2O", "4C", "3P", "6C", "7C" };
 
-            var analisador = new AnalisadorDeVencedor();
-
-            var vencedor = analisador.Analisar(cartasJogador1, cartasJogador2);
-
-            Assert.Equal(vencedorEsperado, vencedor);
-        }
-        [Fact]
-        public void DeveAnalisarVencedrorQuandoJogador2TiverMaiorCarta()
-        {
-            const string vencedorEsperado = "Jogador 2";
-            var cartasJogador1 = new List<string> { "2O", "4C", "3P", "6C", "7C" };
-            var cartasJogador2 = new List<string> { "3O", "5C", "2E", "9C", "7P" };
+            var cartasJogador1 = cartasDoJogador1String.Split(',').ToList();
+            var cartasJogador2 = cartasDoJogador2String.Split(',').ToList();
 
             var analisador = new AnalisadorDeVencedor();
 
